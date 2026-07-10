b9baa84f28c6b90326d570492a0a0aa1c950e6df
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 09:54:08 +0900

    start

---
+++ site.config.js
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  siteName: 'kt상품권 희망티켓',
+  siteUrl: 'https://gift-hh-2.pages.dev',
+  siteDescription: 'kt상품권 실시간 안전 매입. 케이티 모바일 기프티콘과 상품권을 투명한 수수료로 즉시 매입 처리해 드립니다.',
+  author: '희망티켓 010-9469-1051',
+  defaultImage: '/images/sample.png',
+  naverVerificationCode: '',
+  googleVerificationCode: '',
+  company: '희망티켓',
+  phone: '01094691051',
+  sms: '01094691051',
+  kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-',
+  floatingButtons: {
+    phone: 'tel:01094691051',
+    sms: 'sms:01094691051',
+    kakao: 'http://qr.kakao.com/talk/zrNCO4F1ucxqNV7PA9CStjIJt3A-'
+  }
+};
