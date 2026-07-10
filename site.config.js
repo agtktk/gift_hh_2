fcac9e24e6f317de366d1f30fbdd890ae1bac013
alltk <97291524+alltk@users.noreply.github.com>
2026-07-10 11:50:22 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ export const siteConfig = {
   siteDescription: 'kt상품권 실시간 안전 매입. 케이티 모바일 기프티콘과 상품권을 투명한 수수료로 즉시 매입 처리해 드립니다.',
   author: '희망티켓 010-9469-1051',
   defaultImage: '/images/sample.png',
-  naverVerificationCode: '',
+  naverVerificationCode: '4173b828509704ba36c316aefe84c9a19efa3b71',
   googleVerificationCode: '',
   company: '희망티켓',
   phone: '01094691051',
